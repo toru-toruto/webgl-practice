e48338cd1b1705375e93cb2058dab543630c7e44
root <root@DESKTOP-TNEHEAF.localdomain>
2022-11-25 05:26:58 +0900

    scroll

--- pages/scroll/index.tsx
+++ pages/scroll/index.tsx
@@ -19,26 +19,33 @@ const ScrollPage: NextPage = () => {
   const isScrollToggle = useCallback((e: WheelEvent) => {
     if (isRunning.current) return;
     isRunning.current = true;
-    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
     const newSpeed = speed + e.deltaY * 0.0003;
     setSpeed(newSpeed);
     console.log('newSpeed:' + newSpeed);
-    requestAnimationFrame(() => {
-      isRunning.current = false;
-      if (!block.current) return;
-
-      console.log('hoge');
-      // setIsDisplay(scrollTop > 100);
-      let newPosition = position + speed;
-      setSpeed(speed * 0.99);
-      const rounded = Math.round(newPosition);
-      const diff = rounded - newPosition;
-      newPosition += diff * 0.015;
-      setPosition(newPosition);
-      console.log(newPosition);
-      block.current.style.transform = `translate(0,${newPosition * 100}px)`;
-    });
   }, []);
+
+  useEffect(() => {
+    // requestAnimationFrame(() => {
+    // isRunning.current = false;
+    if (!block.current) return;
+    if (!isRunning.current) return;
+
+    let newPosition = position + speed;
+    const rounded = Math.round(newPosition);
+    const diff = rounded - newPosition;
+    newPosition += diff * 0.015;
+
+    const newSpeed = speed * 0.99;
+    console.log('currentSpeed: ' + newSpeed);
+    if (Math.abs(newSpeed) < 0.01) {
+      isRunning.current = false;
+    }
+    setSpeed(newSpeed);
+    setPosition(newPosition);
+    console.log('newPosition:' + newPosition);
+    block.current.style.transform = `translate(0,${newPosition * 100}px)`;
+    // });/
+  }, [speed]);
 
   useEffect(() => {
     document.addEventListener('wheel', isScrollToggle, {
@@ -50,14 +57,14 @@ const ScrollPage: NextPage = () => {
   }, []);
 
   const onClickClose = () => {
-    document.removeEventListener('scroll', isScrollToggle);
+    document.removeEventListener('wheel', isScrollToggle);
     setIsDisplay(false);
   };
 
   return (
     <div>
       {/* <Canvas orthographic></Canvas> */}
-      <div className={'w-full h-[200vh] bg-green-100'}>
+      <div className={'w-full h-[100vh] bg-green-100'}>
         <div ref={block} className={'w-20 h-20 bg-red-100'}></div>
       </div>
     </div>
